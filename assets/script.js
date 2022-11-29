c55fd8fdcb7adba039d118afbb0c379d5b4d3fa1
Kristin Brewer <kr527106@gmail.com>
2022-11-29 15:54:55 -0500

    fixed js link, started js code for search

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,21 @@
-var APIkey = "c117587cfcafa1253c62bb6dccbc1226";
-var city;
-
+//var APIkey = "c117587cfcafa1253c62bb6dccbc1226";
+//var city;
 //https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+var searchFormEl = document.querySelector('#search-form');
+
+function handleSearchFormSubmit(event) {
+    event.preventDefault();
+  
+    var searchInputVal = document.querySelector('#search-input').value;
+  
+    if (!searchInputVal) {
+      console.error('You need a search input value!');
+      return;
+    }
+  else (console.log);
+ //Figure out how to assign value with APIs
+  }
+  
+  searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+  
