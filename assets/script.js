9b1ffde502bcf59ccf4dede4afd5a4baf4c4a8b5
Kristin Brewer <kr527106@gmail.com>
2022-12-03 19:23:50 -0500

    added local storage for searched cities and appended buttons

--- assets/script.js
+++ assets/script.js
@@ -5,11 +5,12 @@ var APIkey = "c117587cfcafa1253c62bb6dccbc1226";
 var searchFormEl = document.querySelector('#search-form');
 var searchInputVal = document.querySelector('#search-input');
 var SpotlightEl = document.querySelector("#spotlight");
+var citylist = document.querySelector("#citybuttonlist");
 
 //added date to spotlight
 function displayDate(){
     var todaydate = dayjs().format ('MMM DD, YYYY');
-    SpotlightEl.textContent = todaydate + " " + "Today's weather is " + displaycity; //need to add weather text
+    SpotlightEl.textContent = todaydate + " " + "Today's weather is "; //need to add weather text
     
 }
 //calling displayed time
@@ -17,23 +18,32 @@ displayDate();
 
 //Submit button ID search
 //Click event listener 
-searchFormEl.addEventListener('click', handleSearchFormSubmit);
+searchFormEl.addEventListener('submit', handleSearchFormSubmit);
 function handleSearchFormSubmit(event) {
     event.preventDefault();
-    console.log(searchInputVal.value);
-    getWeather(searchInputVal.value);
-    localStorage.setItem("city", JSON.stringify(searchInputVal));
-    var displaycity = JSON.parse(localStorage.getItem("city"));
+    const citysearched = searchInputVal.value;
+    window.localStorage.setItem('city', JSON.stringify(citysearched));
+    console.log(citysearched);
+    //SpotlightEl.innerHTML = citysearched;  trial 
 
+  // if (!searchInputVal.value) {
+     // console.error('You need a search input value!');
+   //  return;
+  // }
+ //getWeather(searchInputVal.value);
+ //return;
+    createSearchBtn ();
   }
 
-//adding new search button for city
-
-function createSearchBtn () {
-    searchFormEl.createElement('li');
-    
-}
-
+  //adding search buttons for city
+  function createSearchBtn () {
+    const btn = document.createElement('button');
+    btn.innerText = "New Button"
+  citylist.appendChild(btn);
+    btn.innerText = window.localStorage.getItem('city');
+    return btn; 
+  }
+  
 
 function getWeather (cityname) {
 //gets weather
