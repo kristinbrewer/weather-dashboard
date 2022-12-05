7e2afe9b8cec72f23c3c8d22e8d7572bd50d4569
Kristin Brewer <kr527106@gmail.com>
2022-12-05 08:52:56 -0500

    added loop for dates, pulled temperature for spotlight el, removed quotes from button

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,36 @@
 var APIkey = "c117587cfcafa1253c62bb6dccbc1226";
-//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-
 
 var searchFormEl = document.querySelector('#search-form');
 var searchInputVal = document.querySelector('#search-input');
 var SpotlightEl = document.querySelector("#spotlight");
 var citylist = document.querySelector("#citybuttonlist");
-var day1 = document.querySelector("#day1");
-var day2 = document.querySelector("#day2");
-var day3 = document.querySelector("#day3");
-var day4 = document.querySelector("#day4");
-var day5 = document.querySelector("#day5");
+var card = document.querySelectorAll(".card");
+var carddateEL = document.querySelectorAll(".carddate");
+var cardtempEL = document.querySelectorAll(".cardtemp");
+var cardhumidityEL =document.querySelectorAll(".cardhumidity");
+var cardwindEL = document.querySelectorAll(".cardwind");
+var cardsymbolEL = document.querySelectorAll(".cardsymbol");
+var todaydate = dayjs();
+
+//icon code "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png" 
+//data.weather[0].icon is the code entered 
 
 //added date to spotlight
 function displayDate(){
-    var todaydate = dayjs().format ('MMM DD, YYYY');
-    SpotlightEl.textContent = todaydate + " " + "Today's weather is "; //need to add weather text
-    //var day1 = dayjs().format ('MMM DD, YYYY'); ????
-    day1.textContent = dayjs().isAfter(todaydate);
+    SpotlightEl.textContent = todaydate.format ('MMM DD, YYYY') + " " + "Today's weather is "; //need to add weather text
 }
 //calling displayed time
 displayDate();
+
+//////////////////
+
+//for loop for dates in cards
+for (let i = 1; i < 6; i++) {
+    var carddateEL = document.querySelector("#carddate" + i);
+    var newdate = todaydate.add(i, 'day');
+    carddateEL.textContent = newdate.format ('MMM DD, YYYY')
+}
+console.log(newdate); ////????
 
 //Submit button ID search
 //Click event listener 
@@ -28,25 +38,25 @@ searchFormEl.addEventListener('submit', handleSearchFormSubmit);
 function handleSearchFormSubmit(event) {
     event.preventDefault();
     const citysearched = searchInputVal.value;
-    window.localStorage.setItem('city', JSON.stringify(citysearched));
+    window.localStorage.setItem('city', citysearched);
     console.log(citysearched);
     getWeather(searchInputVal.value);
- return;
     createSearchBtn ();
+    return;
   }
 
   //adding search buttons for city
+  //add event listner for button to store data and call getweather by passing in the text of the button 
   function createSearchBtn () {
     const btn = document.createElement('button');
-    btn.innerText = "New Button"
+    btn.innerText = "New Button";
   citylist.appendChild(btn);
     btn.innerText = window.localStorage.getItem('city');
     return btn; 
   }
   
-
-function getWeather (cityname) {
 //gets weather
+function getWeather (cityname) {
 console.log(cityname);
 var URLAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&appid=" + APIkey +"&units=imperial";
 
@@ -58,5 +68,32 @@ fetch(URLAPI)
 .then(function(data){
 console.log("THIS IS DATA: ", data)
 console.log(data.main.temp)
+window.localStorage.setItem('temp', JSON.stringify(data.main.temp));
+SpotlightEl.innerHTML = "";
+var tempEL = document.createElement("p");
+tempEL.textContent = "temperature " + data.main.temp;
+
+SpotlightEl.append(tempEL);
+forecast(data.coord.lon,data.coord.lat)
 })
+}
+
+
+function forecast (longitude, latitude){
+    var URLAPI = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude+ "&appid=" + APIkey +"&units=imperial";
+
+    fetch(URLAPI)
+    .then(function(response){
+        console.log("THIS IS RESPONSE: ",response)
+        return response.json()
+    })
+    .then(function(data){
+        console.log(data);
+        for (var i= 0; i <data.list.length; i++) {
+            if (data.list[i].dt_txt.indexOf("12:00:00") > 0){
+                console.log(data.list[i])
+            }
+        }
+    })
+
 }
